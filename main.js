1f9a51aab604cc682c132d7bd260661d376145ae
tariqahmed6706 <tariq.ahmed6706@gmail.com>
2025-12-04 22:36:26 +0200

    4Dec2025

--- main.js
+++ main.js
@@ -2,62 +2,46 @@
 
 function openMenu() {
   const menu = document.getElementById("header-menu");
-  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
-  const close_feedback_with_menu = document.getElementById("Feedback-Popup");
-  if (close_feedback_with_menu.style.display === "grid") {
-    close_feedback_with_menu.style.display = "none";
+  if(menu.style.display = menu.style.display === "flex" ) {
+    menu.style.display = menu.style.display = "none";
+  } else {
+    menu.style.display = menu.style.display = "flex";
   }
 }
-
-// for header button(a)
 
 document.querySelectorAll(".my-link").forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     link.classList.add("clicked");
-
-    // بعد نص ثانية نشيل التأثير
     setTimeout(() => {
       link.classList.remove("clicked");
-
-      // بعد 200 ملي ثانية كمان نعمل التحويل
       setTimeout(() => {
         window.location.href = link.getAttribute("href");
       }, 25);
-    }, 200); // التأثير يفضل نص ثانية
+    }, 200);
   });
 });
-
 function openfeedback() {
   document.getElementById("Feedback-Popup").style.display = "grid";
 }
 function closefeedback() {
   document.getElementById("Feedback-Popup").style.display = "none";
 }
-
 function switchtolightmode() {
   
 }
 function switchtodarkmode() {
   
 }
-
-// المودال و زرار القفل
 const bookingModal = document.getElementById("booking-modal");
-// نجيب كل الأزرار اللي بتفتح المودال
 const bookingButtons = document.querySelectorAll(".booking-btn");
 
-// لما ندوس على أي زرار
 bookingButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
-
-    // تأثير صغير على الزرار
     btn.classList.add("clicked");
-
     setTimeout(() => {
       btn.classList.remove("clicked");
-      // نظهر المودال
       bookingModal.style.display = "flex";
     }, 150);
   });
